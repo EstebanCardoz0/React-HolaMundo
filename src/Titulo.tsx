777cda1d7a60e46625ff4bc4b5afd6e5297299c5
Esteban Cardozo <esteban.cardozo.wec@gmail.com>
2025-04-11 20:46:30 -0300

    intro

---
+++ src/Titulo.tsx
@@ -0,0 +1,10 @@
+function Titulo(){
+    //esto es jsx 
+    const nombre = " Javier";
+    if (nombre) {
+return <h1>Hola {nombre} </h1>
+
+    } 
+    return <p>Hola Mundo</p>;
+}
+export default Titulo;
